69cb3fad6505909e92d3effd2809d4c5d5526130
ccantey <cantey0701@gmail.com>
2017-05-23 11:28:06 -0500

    ui early

--- js/app.js
+++ js/app.js
@@ -1,8 +1,21 @@
 function init(){
+	southWest = new mapboxgl.LngLat( -104.7140625, 41.86956);
+    northEast = new mapboxgl.LngLat( -84.202832, 50.1487464);
+    //bounds = new mapboxgl.LngLatBounds(southWest, northEast);
+    bounds = new mapboxgl.LngLatBounds(southWest,northEast);
+
+
 	mapboxgl.accessToken = 'pk.eyJ1IjoiY2NhbnRleSIsImEiOiJjaWVsdDNubmEwMGU3czNtNDRyNjRpdTVqIn0.yFaW4Ty6VE3GHkrDvdbW6g';
 	var map = new mapboxgl.Map({
 	    container: 'map',
-	    style: 'mapbox://styles/mapbox/streets-v9'
+	    style: 'mapbox://styles/mapbox/light-v9',
+		center: [-93.6678,46.50],
+		maxBounds:bounds,		
+		zoom: 6
+	    //style: 'mapbox://styles/mapbox/streets-v9'
 	});
+
+	var nav = new mapboxgl.NavigationControl({position: 'top-right'}); // position is optional
+    map.addControl(nav);
 }
 
--- js/helper.js
+++ js/helper.js
@@ -1,3 +1,7 @@
 $( document ).ready(function() {
     init();
+
+    $('[data-toggle="offcanvas"]').click(function () {
+    	$('.row-offcanvas').toggleClass('active')
+  	});
 });
